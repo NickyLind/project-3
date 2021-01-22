9b7b5b1c90a1e35e8dbd59015a7a2953997f0e2f
Nick Lindau <nicholaithegreat@gmail.com>
2021-01-22 15:12:10 -0800

    Add the ability to input zero into the loop and add some styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function()  {
   $("#form").submit(function(event) {
     event.preventDefault();
-    
-    
 
 let number = $("#input").val();
 let userArray = [];
 for (let i = 0; i <= number; i++) {
   userArray.push(i)
   if(i.toString().includes("3"))	{
-  userArray.splice([i], i, "Are You My Neighbor");
-} else if (i.toString().includes("2"))	
+  userArray.splice([i], i, "Won't You Be My Neighbor?");
+} else if (i.toString().includes("2"))	{
   userArray.splice([i], i, "Boop!");	
- else if (i.toString().includes("1"))	
-   userArray.splice([i], i, "Beep!");	 
+} else if (i.toString().includes("1"))	{
+  userArray.splice([i], i, "Beep!");
+} else {
+  userArray.push[i] }
 } 
-$("#output").text(userArray);
- 
-});
+userStrArray = userArray.join(",  ")
+$("#output").text(userStrArray); 
+  });
 });
