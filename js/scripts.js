18cdce9ca6ea494e4de4931ae1c004655475e524
Nick Lindau <nicholaithegreat@gmail.com>
2021-01-22 10:03:38 -0800

    Clear fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,9 @@ $(document).ready(function()  {
 //1~ console.log(!isNaN(2));
 //2~ console.log(number>0);
 //3~  console.log(number % 1 !== 0)
+//4~ let userNumber = 4
+//   let userArray = [];
+//    for (let i = 0; i <= userNumber; i++)
+//    console.log(i)
+
+    
