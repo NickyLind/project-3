da6e8035cdbe4694654858e9ba6bf2604e74faf4
Nick Lindau <nicholaithegreat@gmail.com>
2021-01-27 16:35:37 -0800

    Seperate UI and Business logic and fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
-$(document).ready(function()  {
-  $("#form").submit(function(event) {
+// Business Logic
+function BeepBoop(number) {
+  let userArray = [];
+  for (let i = 0; i <= number; i++) {
+    userArray.push(i)
+    if (i.toString().includes("3")) {
+      userArray.splice([i], i, "Won't You Be My Neighbor?");
+    } else if (i.toString().includes("2")) {
+      userArray.splice([i], i, "Boop!");
+    } else if (i.toString().includes("1")) {
+      userArray.splice([i], i, "Beep!");
+    } else {
+      userArray.push[i]
+    }
+    userStrArray = userArray.join(",  ")
+  }
+  return userArray;
+}
+
+// UI Logic
+$(document).ready(function () {
+  $("#form").submit(function (event) {
     event.preventDefault();
-
-let number = $("#input").val();
-let userArray = [];
-for (let i = 0; i <= number; i++) {
-  userArray.push(i)
-  if(i.toString().includes("3"))	{
-  userArray.splice([i], i, "Won't You Be My Neighbor?");
-} else if (i.toString().includes("2"))	{
-  userArray.splice([i], i, "Boop!");	
-} else if (i.toString().includes("1"))	{
-  userArray.splice([i], i, "Beep!");
-} else {
-  userArray.push[i] }
-} 
-userStrArray = userArray.join(",  ")
-
-$("#hidemefirst").hide();
-$("#hidemefirst").fadeIn(3000);
-$("#output").text(userStrArray); 
+    const number = $("#input").val();
+    let userStrArray = BeepBoop(number)
+    $("#hidemefirst").hide();
+    $("#hidemefirst").fadeIn(3000);
+    $("#output").text(userStrArray);
   });
 });
